3e414febe6b72795821793083ea6497954f791a5
Stephanie L. Mason <masons7@LabImage2016-17>
2017-01-18 14:44:38 -0800

    initial commit

---
+++ Assignment1.java
@@ -0,0 +1,16 @@
+/*============================================================================/*
+| Assignment1.java                                                             |
+|                                                                              |
+| CSCI 330 - Winter 2017                                                       |
+|                                                                              |
+| Given a tsv input file containing stock market data, this program identifies:|
+| * crazy days (when there are high price fluctuations in a single day)        |
+| * craziest days (the day for which the price fluctuation was highest)        |
+| * stock splits of 2:1, 3:1, and 3:2 ratios                                   |
+|                                                                              |
+| by Stephanie Mason                                                           |
+/*============================================================================*/
+
+public static void main(String[] args) {
+  
+}
